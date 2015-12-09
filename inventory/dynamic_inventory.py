6d3fbfefe5fbbc9cb57312466412ffbcd1bf0160
Miguel Alex Cantu <miguel.cantu@rackspace.com>
2015-12-09 22:12:00 +0000

    Added bit to add an external cinder node
    Added to dynamic inventory and added playbook

--- inventory/dynamic_inventory.py
+++ inventory/dynamic_inventory.py
@@ -31,14 +31,29 @@ def main():
 		compute_name = "compute" + str(compute_num)
 		dynamic_inventory["_meta"]["hostvars"][compute_name] = _build_compute_dict(compute_ip, conf_dict['network'])
 		compute_num += 1
+
+	cinder_num = 1
+	try:
+		for cinder_ip in conf_dict["nodes"]["cinder"]["ips"]:
+			cinder_name = "cinder" + str(cinder_num)
+			dynamic_inventory["_meta"]["hostvars"][cinder_name] = _build_cinder_hostvar_dict(
+				cinder_ip,
+				conf_dict['network'],
+				conf_dict['nodes']['cinder']['backend_configurations'])
+			cinder_num += 1
+	except KeyError:
+		print "No cinder configurations found in /etc/oscar/oscar.conf. Using the controller for cinder block-storage"
 	
 	# Add mgmt_ip for each compute to the controller dict
 	# and build out host groups
 	dynamic_inventory['compute_nodes'] = { 'hosts': [] }
+	dynamic_inventory['cinder_nodes'] = { 'hosts': [] }
 	for host_name, variables in dynamic_inventory["_meta"]["hostvars"].iteritems():
 		if 'compute' in host_name:
 			dynamic_inventory['_meta']['hostvars']['controller'][host_name + '_mgmt_ip'] = variables['compute_mgmt_ip'] 
 			dynamic_inventory['compute_nodes']['hosts'].append(host_name)
+		elif 'cinder' in host_name:
+			dynamic_inventory["cinder_nodes"]["hosts"].append(host_name)
 		elif 'controller' in host_name:
 			dynamic_inventory[host_name] = { "hosts": ["controller"] }
 
@@ -63,6 +78,24 @@ def _build_compute_dict(compute_ip, network_dict):
 	starting_ip_for_bridges += 1
 
 	return compute_dict
+
+def _build_cinder_hostvar_dict(cinder_ip, network_dict, backend_config_dict):
+	"""
+	This method builds the hostvar dictionary for cinder host 'cinder_ip'.
+	"""
+	global starting_ip_for_bridges
+	cinder_hostvar_dict = {}
+	cinder_hostvar_dict['ansible_ssh_host'] = cinder_ip
+
+	ip_allocations = _assign_ip_to_bridges_from_network(network_dict)
+
+	cinder_hostvar_dict["cinder_mgmt_ip"] = ip_allocations['mgmt_ip']
+	cinder_hostvar_dict["cinder_vxlan_ip"] = ip_allocations['vxlan_ip']
+	cinder_hostvar_dict["cinder_storage_ip"] = ip_allocations['storage_ip']
+	cinder_hostvar_dict["backend_config"] = backend_config_dict
+
+	return cinder_hostvar_dict
+	
 
 
 def _build_controller_dict(conf_dict):
